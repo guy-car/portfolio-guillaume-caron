a7d5d07ecce6ab0a025e3f1376891f0b769a05ee
guy-car <guil.crn@gmail.com>
2025-08-06 19:55:18 -0400

    dd mobile responsiveness

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Footer() {
   return (
     <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+      <ul className="font-sm mt-8 flex flex-row space-x-4 text-neutral-600 dark:text-neutral-300">
 
         <li>
           <a
